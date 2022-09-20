0ca837ec3ff3a899a5031011cee87380fec0a7e6
Yuriy Novikov <tow1987@yandex.ru>
2022-09-20 15:30:04 +0300

    Create new project with Spring Initializr

---
+++ src/main/java/com/nov/checkyourconsumablesapi/CheckYourConsumablesApiApplication.java
@@ -0,0 +1,13 @@
+package com.nov.checkyourconsumablesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckYourConsumablesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckYourConsumablesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nov/checkyourconsumablesapi/CheckYourConsumablesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nov.checkyourconsumablesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckYourConsumablesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
